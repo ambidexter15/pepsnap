0998b45d96cc9498477d5e29d048252146d7bfed
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-05-11 21:51:58 +0530

    live-streaming

---
+++ script.js
@@ -0,0 +1,13 @@
+let video = document.querySelector("video");
+
+let constraint = {
+    video : true,
+    audio : false
+}
+
+navigator.mediaDevices.getUserMedia(constraint)
+.then((stream) => 
+{
+  video.srcObject = stream
+})
+
